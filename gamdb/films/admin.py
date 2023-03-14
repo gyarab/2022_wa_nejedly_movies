a27fa54aa1ffaf16dcb19abb77bd3c20fb65ff25
Vojtech <vojtech.nejedly@student.gyarab.cz>
2023-03-14 09:12:58 +0100

    14.3.

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Movie
 # Register your models here.
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'year']
+    list_display_links = ['name']
+
+admin.site.register(Movie, MovieAdmin)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -4,3 +4,6 @@ class Movie(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
     description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}({self.year})"
